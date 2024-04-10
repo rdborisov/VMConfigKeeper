2634e37ef3284eb343b6c2a5fbe993644bbf53a0
ruslan borisov <rdborisov99@gmail.com>
2024-04-10 22:24:50 +0300

    form experiment

--- VMConfigKeeper/myprojectenv/temp_cpu/forms.py
+++ VMConfigKeeper/myprojectenv/temp_cpu/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    public_user = forms.CharField(max_length=255)
+    content = forms.CharField(widget=forms.Textarea())
+    file = forms.FileField()
+    is_published = forms.BooleanField()
+
